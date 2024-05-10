109e4f9509f90d281f4b5648475592cb5ed1856c
sanchoo1 <sanchowong1@gmail.com>
2024-05-10 03:12:29 +0200

    67 train

---
+++ yoloviii/detect.py
@@ -0,0 +1,41 @@
+from ultralytics import YOLO
+import cv2
+from pathlib import Path
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]
+# Load a  model
+model = YOLO( ROOT / 'best.pt')
+
+# Open the video
+source = 3
+cap = cv2.VideoCapture(source)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 inference on the frame
+        results = model(frame)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        # View results
+        for r in results:
+            boxes = r.boxes
+            if boxes.xyxy.numel() > 0:
+                for box in boxes.xyxy:
+                    x1, y1, x2, y2 = box.tolist()
+                    print(x1, y1, x2, y2)
+
+              # print the Boxes object containing the detection bounding boxes
+
+        if cv2.waitKey(1) == 'q':
+            break
+
---
+++ yolovv/datasetd/README.dataset.txt
@@ -0,0 +1,6 @@
+# pen > pen-version2
+https://universe.roboflow.com/yolov5-h3oo1/pen-zyj9j
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
